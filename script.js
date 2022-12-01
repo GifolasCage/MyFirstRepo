b18e0bb73d1526f308f6611693052db08ac98c37
GifolasCage <jorgen.eikemo@gmail.com>
2022-12-01 19:58:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function myFunction(){
     let myHeader = document.getElementById("myHeader");
     myHeader.innerHTML = "A New Kind of Header!";
     myHeader.className = "classHead";
+    console.log("hello!");
 }
