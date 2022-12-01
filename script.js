dc46e4e6481e0842bda364c686c82618b3276a88
jorgen eikemo <jorgen.eikemo@gmail.com>
2022-12-01 20:13:16 +0100

    anotherone

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function myFunction(){
     let myHeader = document.getElementById("myHeader");
     myHeader.innerHTML = "A New Kind of Header!";
     myHeader.className = "classHead";
-    console.log("hello!");
+    console.log("hello errbode!");
 }
